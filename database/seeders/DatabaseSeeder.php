41c253cebadf258b27e87de5562f547639fbfcf5
Why. <wahyuwinanta24@gmail.com>
2025-12-23 19:01:50 +0700

    create seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(RolePermissionSeeder::class);
-        
+        $this->call(PekerjaUserSeeder::class);
+        $this->call(ProjectSeeder::class);
     }
 }
